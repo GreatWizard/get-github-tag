61c3918df82194e2062ea8094f65bf87852e5f83
Guillaume Gérard <guillaume.gerard88@gmail.com>
2019-07-12 15:59:47 +0200

    feat: use regexp match instead of indexOf

--- src/index.js
+++ src/index.js
@@ -6,16 +6,17 @@ const HOST = process.env.GET_TAG_HOST || 'https://api.github.com';
 let __tags__ = [];
 
 module.exports = function(owner, repo, name = '') {
-  let key = `${owner}--${repo}__${name}`;
+  let key = `${owner}--${repo}`;
   if (__tags__[key] === undefined) {
     __tags__[key] = fetch(`${HOST}/repos/${owner}/${repo}/tags`).then(res => res.json());
   }
+  let regexp = new RegExp(name);
   return __tags__[key]
     .then(json => {
       let tag = json.find(tag => {
-        return tag.name.indexOf(name) !== -1;
+        return tag.name.match(regexp) !== null;
       });
-      return tag.name;
+      return tag && tag.name;
     })
     .catch(e => {
       console.log('An error occurred: ', e);
